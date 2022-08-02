5098d4900cbdd6db7d06dfdafbcbfe4d2114551b
Nicola Titmus <nicola.koo@cantab.net>
2022-08-02 23:44:16 +0100

    simple web server

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 provider "aws" {
     region = "eu-west-2"
 }
@@ -5,4 +6,27 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-02ada3b3a79510555"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+    
+    tags = {
+      Name = "terraform-example"
+    }
 }
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
